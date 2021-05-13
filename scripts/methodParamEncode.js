85539a37df264b7d183dd2348ecc5e1a6ac1cc7e
jinwendong <jinwendong@huobi.com>
2021-05-13 15:54:20 +0800

    add bank test

--- scripts/methodParamEncode.js
+++ scripts/methodParamEncode.js
@@ -6,12 +6,12 @@ let MdxStrategyWithdrawMinimizeTrading_calldata_types = ['address', 'address', '
 let MdxGoblin_calldata_types = ['address', 'bytes']
 
 let data = encode(MdxStrategyAddTwoSidesOptimal_calldata_types,
-    '0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047', '0xa71EdC38d189767582C38A3145b5873052c3e47a',0,0,0 )
+    '0x0298c2b32eaE4da002a15f36fdf7615BEa3DA047', '0xa71EdC38d189767582C38A3145b5873052c3e47a',100000000,0,0 )
 console.log(data)
 console.log(decode(MdxStrategyWithdrawMinimizeTrading_calldata_types, data))
 
 let data2 = encode(MdxGoblin_calldata_types,
-    '0xc732ceE78bd5fb04C58aAB3fc5E3F54cC338b63B', data )
+    '0xC5649d098F7e87A0e397fe20e5A5f458d8e401ef', data )
 console.log(data2)
 
 
