df7e5bb1a1020697a71f41d27f244889ac693da9
xiepengqi <xiepengqi@huobi.com>
2021-05-27 15:20:28 +0800

    test

--- test/bank.js
+++ test/bank.js
@@ -3,34 +3,44 @@ const { expect } = require("chai");
 const impersonateAccount = "0xAf4cBbDdd17D1fdAD956663a54eE8960De231348";
 const usdt = "0xa71edc38d189767582c38a3145b5873052c3e47a";
 const husd = "0x0298c2b32eae4da002a15f36fdf7615bea3da047";
-let erc20Artifact = '@openzeppelin/contracts/token/ERC20/IERC20.sol:IERC20'
+let erc20Artifact = '@openzeppelin/contracts/token/ERC20/ERC20.sol:ERC20'
+let signer;
 
-describe("bank", function() {
-    it("test", async function() {
-        const addStra = await $getAddress('MdxStrategyAddTwoSidesOptimal');
-        const bankAddress = await $getContract('Bank');
-        const usdtA = await ethers.getContractAt(erc20Artifact,usdt);
-        const husdA = await ethers.getContractAt(erc20Artifact,husd);
-
+describe("BANK", function() {
+    before('INIT SIGNER',async () => {
         await hre.network.provider.request({
             method: "hardhat_impersonateAccount",
             params: [impersonateAccount]}
         );
+        signer = await ethers.provider.getSigner(impersonateAccount);
 
-        const signer = await ethers.provider.getSigner(impersonateAccount);
+        const usdtA = await ethers.getContractAt(erc20Artifact,usdt);
+        const husdA = await ethers.getContractAt(erc20Artifact,husd);
+        console.log('impersonateAccount:' + impersonateAccount);
         console.log('Balance: ' + (await signer.getBalance()).toString());
-        console.log('USDT: ' + (await usdtA.balanceOf(signer.getAddress())).toString());
-        console.log('HUSD: ' + (await husdA.balanceOf(signer.getAddress())).toString());
+        console.log('USDT: ' + (await usdtA.balanceOf(signer.getAddress())).toString() + ' ' + (await usdtA.decimals()));
+        console.log('HUSD: ' + (await husdA.balanceOf(signer.getAddress())).toString() + ' ' + (await husdA.decimals()));
+    });
+    it('DEPOSIT', async () => {
+        const bankAddress = await $getContract('Bank');
+        const usdtA = await ethers.getContractAt(erc20Artifact,usdt);
+
         //approve bank
-        await usdtA.connect(signer).approve(bankAddress.address,"1000000000000000000000");
-        await husdA.connect(signer).approve(bankAddress.address,"1000000000000000");
-        //approve strategy
-        await usdtA.connect(signer).approve(addStra,"1000000000000000000000");
-        await husdA.connect(signer).approve(addStra,"1000000000000000");
+        await usdtA.connect(signer).approve(bankAddress.address,"3000000000000000000");
 
         //deposit
+        console.log("Bank USDT: " + (await bankAddress.banks(usdt)).totalVal.toString());
         await bankAddress.$connect(signer).$deposit(usdt,"3000000000000000000");
         console.log("Bank USDT: " + (await bankAddress.banks(usdt)).totalVal.toString());
+    });
+    it("开仓", async () => {
+        const addStra = await $getAddress('MdxStrategyAddTwoSidesOptimal');
+        const bankAddress = await $getContract('Bank');
+        const husdA = await ethers.getContractAt(erc20Artifact,husd);
+
+        //approve strategy
+        await husdA.connect(signer).approve(addStra,"100000000");
+
         let cPosition = await bankAddress.$currentPos()
         console.log("currentPosition: " + cPosition)
         await bankAddress.$connect(signer).$opPosition(0,1,"1000000000000000000", $opAddData(addStra, husd, usdt, 100000000, 0));
